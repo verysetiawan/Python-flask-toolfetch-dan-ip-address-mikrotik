a902354f9890c2dbd78725f4400a239698ded21c
verysetiawan <kangphery@gmail.com>
2020-06-02 01:33:35 +0700

    menambahkan render template

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import time
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
 @app.route("/conf", methods=["POST"])
 def config():
@@ -13,13 +13,21 @@ def config():
     print (f"IP Address Mikrotik adalah : {ip_mik}")
 
     #Menyimpan informasi ip ke file ip_address.txt
-    file_write = open ("ip_address.txt","a")
+    file_write = open ("template/ip_address.txt","a")
     file_write.write (ip_mik)
     file_write.write ("\n")
     file_write.close()
 
     return jsonify(data)
-    
+
+@app.route("/index")
+def indexku():
+    file_open = open ("template/ip_address.txt","r")
+    baca_file = file_open.readlines()
+
+    return render_template ("index.html", var=baca_file)  
+
+
 
 if __name__ == "__main__":
     app.run (host='192.168.122.1', debug=True, port=5005)
